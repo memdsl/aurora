5bc805bb451f96e3d18c5f8449d883a018d58b55
myyerrol <myyerrol@126.com>
2024-07-03 20:40:19 +0800

    feat: add mul models

--- src/sv/common/multiplier/sim/mul_xbit_shift_tb.sv
+++ src/sv/common/multiplier/sim/mul_xbit_shift_tb.sv
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 
-module mul_shift_tb();
+module mul_xbit_shift_tb();
 
 initial begin
-    $dumpfile("build/mul_shift.vcd");
-    $dumpvars(0, mul_shift_tb);
+    $dumpfile("build/mul_xbit_shift.vcd");
+    $dumpvars(0, mul_xbit_shift_tb);
 end
 
 parameter CYCLE      = 10;
-parameter DATA_WIDTH = 8;
+parameter DATA_WIDTH = 4;
 
 logic                      w_clk;
 logic                      w_rst_n;
@@ -20,17 +20,17 @@ always #(CYCLE / 2) w_clk = ~w_clk;
 initial begin
     w_clk   = 1'b0;
     w_rst_n = 1'b0;
-    w_num_a = 8'b00001111;
-    w_num_b = 8'b11110000;
+    w_num_a = 4'b1010;
+    w_num_b = 4'b1001;
     #(CYCLE * 1);
     w_rst_n = 1'b1;
     #(CYCLE * 10);
     $finish;
 end
 
-mul_shift #(
+mul_xbit_shift #(
     .DATA_WIDTH(DATA_WIDTH)
-) mul_shift_inst(
+) mul_xbit_shift_inst(
     .i_clk(w_clk),
     .i_rst_n(w_rst_n),
     .i_num_a(w_num_a),
