0a4f478e1e3b69afdadf66899aeb50ad74587e95
myyerrol <myyerrol@126.com>
2024-07-09 17:46:26 +0800

    style: delete old files

--- src/chisel/rtl/src/base/Config.scala
+++
@@ -1,8 +0,0 @@
-package ip.base
-
-import chisel3._
-import chisel3.util._
-
-trait Config {
-
-}
--- src/chisel/rtl/src/perip/hdmi/HDMI.scala
+++
@@ -1,7 +0,0 @@
-package ip.perip
-
-import chisel3._
-import chisel3.util._
-
-class HDMI extends Module {
-}
