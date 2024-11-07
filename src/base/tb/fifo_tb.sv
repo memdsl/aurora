40fe718fb73b6d1360b1450a7dee81d572c41f83
myyerrol <myyerrol@126.com>
2024-11-07 15:35:54 +0800

    feat: update fifo tb

--- src/base/tb/fifo_tb.sv
+++ src/base/tb/fifo_tb.sv
@@ -1,70 +1,85 @@
-//-- modified by xlinxdu, 2022/05/17
-module fifo_tb;
-  reg           rst_n_i  ;
+/*
+ * @Author      : myyerrol
+ * @Date        : 2024-11-02 18:09:48
+ * @LastEditors : myyerrol
+ * @LastEditTime: 2024-11-07 15:35:29
+ * @Description : Tools testbench.
+ *
+ * Copyright (c) 2024 by MEMDSL, All Rights Reserved.
+ */
 
-  reg           wr_clk_i ;
-  reg           wr_en_i  ;
-  reg [15:0]    wr_data_i;
+`timescale 1ns / 1ps
 
-  reg           rd_clk_i ;
-  reg           rd_en_i  ;
-  wire [15:0]   rd_data_o;
-
-  reg           full_o   ;
-  reg           empty_o  ;
-
-initial begin
-  rst_n_i  = 1;
-  wr_clk_i = 0;
-  wr_en_i  = 0;
-  wr_data_i= 16'b0;
-  rd_clk_i = 0;
-  rd_en_i  = 0;
-
-  # 1 rst_n_i = 0;
-  # 2 rst_n_i = 1;
-end
-
-initial begin
-  #20 wr_en_i = 1;
-      rd_en_i = 0;
-  #40 wr_en_i = 0;
-      rd_en_i = 1;
-  #30 wr_en_i = 1 ;
-      rd_en_i = 0 ;
-  #13 rd_en_i = 1 ;
-  #10
-  repeat(100)
-  begin
-      #5 wr_en_i = {$random}%2 ;
-         rd_en_i = {$random}%2 ;
-    end
-end
-
-always #1.5 wr_clk_i  = ~wr_clk_i ;
-always #1   rd_clk_i  = ~rd_clk_i ;
-always #3   wr_data_i = {$random}%16'hFF;
-
-fifo_mode_a #(
-    .DATA_WIDTH(16),
-    .FIFO_DEPTH(8)
-) u_async_fifo(
-                         .i_wr_rst_n(rst_n_i  ),
-                         .i_wr_clk  (wr_clk_i ),
-                         .i_wr_en   (wr_en_i  ),
-                         .i_wr_data (wr_data_i),
-                         .i_rd_rst_n(rst_n_i  ),
-                         .i_rd_clk  (rd_clk_i ),
-                         .i_rd_en   (rd_en_i  ),
-                         .o_rd_data (rd_data_o),
-                         .o_wr_full (full_o   ),
-                         .o_rd_empty(empty_o  )
-                       );
+module fifo_tb();
 
 initial begin
     $dumpfile("build/fifo.vcd");
     $dumpvars(0, fifo_tb);
+end
+
+parameter DATA_WIDTH = 16;
+
+logic                      w_rst_n;
+logic                      w_wr_clk;
+logic                      w_wr_en;
+logic [DATA_WIDTH - 1 : 0] w_wr_data;
+logic                      w_rd_clk;
+logic                      w_rd_en;
+
+// Cycle =  3ns, Freq = 300MHz
+always #1.5 w_wr_clk  = ~w_wr_clk;
+// Cycle =  2ns, Freq = 200MHz
+always #1   w_rd_clk  = ~w_rd_clk;
+always #3   w_wr_data = {$random} % {DATA_WIDTH{1'b1}};
+
+initial begin
+    w_rst_n  = 1'b1;
+
+    w_wr_clk = 1'b0;
+    w_wr_en  = 1'b0;
+    w_wr_data= {DATA_WIDTH{1'b0}};
+
+    w_rd_clk = 1'b0;
+    w_rd_en  = 1'b0;
+
+    # 1 w_rst_n = 0;
+    # 2 w_rst_n = 1;
+
+
+
+    #20 w_wr_en = 1'b1;
+        w_rd_en = 1'b0;
+    #40 w_wr_en = 1'b0;
+        w_rd_en = 1'b1;
+    #30 w_wr_en = 1'b1;
+        w_rd_en = 1'b0;
+    #13 w_rd_en = 1'b1;
+    #10
+
+    repeat(100)
+    begin
+    #5 w_wr_en = {$random}%2 ;
+        w_rd_en = {$random}%2 ;
+    end
+
     #1000 $finish;
 end
+
+fifo_mode_a #(
+    .DATA_WIDTH(DATA_WIDTH),
+    .FIFO_DEPTH(8)
+) u_async_fifo(
+    .i_wr_clk  (w_wr_clk),
+    .i_wr_rst_n(w_rst_n),
+    .i_wr_en   (w_wr_en),
+    .i_wr_data (w_wr_data),
+    .o_wr_full (),
+
+    .i_rd_clk  (w_rd_clk),
+    .i_rd_rst_n(w_rst_n),
+    .i_rd_en   (w_rd_en),
+    .o_rd_data (),
+    .o_rd_empty()
+);
 
 endmodule
