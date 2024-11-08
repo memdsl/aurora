9f15e02dcf5de21ef3b6b8d6eea18d8819458dd2
myyerrol <myyerrol@126.com>
2024-11-09 00:49:37 +0800

    feat: finish fifo sync

--- src/base/rtl/fifo.sv
+++ src/base/rtl/fifo.sv
@@ -2,7 +2,7 @@
  * @Author      : myyerrol
  * @Date        : 2024-11-02 15:22:27
  * @LastEditors : myyerrol
- * @LastEditTime: 2024-11-08 11:12:53
+ * @LastEditTime: 2024-11-09 00:47:25
  * @Description : First Input First Out
  *
  * Copyright (c) 2024 by MEMDSL, All Rights Reserved.
@@ -30,7 +30,103 @@ interface fifo_io();
     // );
 endinterface
 
-// Async FIFO
+// FIFO Synchronization
+module fifo_mode_s #(
+    parameter DATA_WIDTH = 32,
+    parameter FIFO_DEPTH =  8,
+    parameter PTRS_WIDTH = $clog2(FIFO_DEPTH)
+) (
+    input  logic                      i_clk,
+    input  logic                      i_rst_n,
+
+    input  logic                      i_wr_en,
+    input  logic [DATA_WIDTH - 1 : 0] i_wr_data,
+    output logic                      o_wr_full,
+
+    input  logic                      i_rd_en,
+    output logic [DATA_WIDTH - 1 : 0] o_rd_data,
+    output logic                      o_rd_empty
+);
+
+    logic [DATA_WIDTH - 1 : 0] r_sram[FIFO_DEPTH - 1 : 0];
+
+    logic [PTRS_WIDTH - 1 : 0] r_wr_ptr;
+    logic [PTRS_WIDTH - 1 : 0] r_rd_ptr;
+
+    logic [PTRS_WIDTH     : 0] r_cnt;
+
+    // Counter
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            r_cnt <= {(PTRS_WIDTH + 1){1'b0}};
+        end
+        else if (i_wr_en && !o_wr_full && i_rd_en && !o_rd_empty) begin
+            r_cnt <= r_cnt;
+        end
+        else if (i_wr_en && !o_wr_full) begin
+            r_cnt <= r_cnt + 1'b1;
+        end
+        else if (i_rd_en && !o_rd_empty) begin
+            r_cnt <= r_cnt - 1'b1;
+        end
+        else begin
+            r_cnt <= r_cnt;
+        end
+    end
+
+    // Write operations
+    reg_rst_y_mode_a_en_y #(
+        .DATA_WIDTH(PTRS_WIDTH)
+    ) u_reg_wr_ptr(
+        .i_clk  (i_clk),
+        .i_rst_n(i_rst_n),
+        .i_en   (i_wr_en && !o_wr_full),
+        .i_data (r_wr_ptr + 1'b1),
+        .o_data (r_wr_ptr)
+    );
+
+    integer i;
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            for (i = 0; i < FIFO_DEPTH; i++) begin
+                r_sram[i] <= {DATA_WIDTH{1'b0}};
+            end
+        end
+        else if (i_wr_en && !o_wr_full) begin
+            r_sram[r_wr_ptr] <= i_wr_data;
+        end
+        else begin
+            r_sram[r_wr_ptr] <= r_sram[r_wr_ptr];
+        end
+    end
+
+    assign o_wr_full = (r_cnt === FIFO_DEPTH) ? 1'b1 : 1'b0;
+
+    // Read operations
+    reg_rst_y_mode_a_en_y #(
+        .DATA_WIDTH(PTRS_WIDTH)
+    ) u_reg_rd_ptr(
+        .i_clk  (i_clk),
+        .i_rst_n(i_rst_n),
+        .i_en   (i_rd_en && !o_rd_empty),
+        .i_data (r_rd_ptr + 1'b1),
+        .o_data (r_rd_ptr)
+    );
+
+    reg_rst_y_mode_a_en_y #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) u_reg_rd_data(
+        .i_clk  (i_clk),
+        .i_rst_n(i_rst_n),
+        .i_en   (i_rd_en && !o_rd_empty),
+        .i_data (r_sram[r_rd_ptr]),
+        .o_data (o_rd_data)
+    );
+
+    assign o_rd_empty = (r_cnt === 0) ? 1'b1 : 1'b0;
+endmodule
+
+// FIFO Asynchronization
 module fifo_mode_a #(
     parameter DATA_WIDTH = 32,
     parameter FIFO_DEPTH =  8,
@@ -170,98 +266,4 @@ module fifo_mode_a #(
         .i_data (r_sram[w_rd_addr]),
         .o_data (o_rd_data)
     );
-endmodule
-
-// Sync FIFO
-module fifo_mode_s #(
-    parameter DATA_WIDTH = 32,
-    parameter FIFO_DEPTH =  8,
-    parameter PTRS_WIDTH = $clog2(FIFO_DEPTH)
-) (
-    input  logic                      i_clk,
-    input  logic                      i_rst_n,
-
-    input  logic                      i_wr_en,
-    input  logic [DATA_WIDTH - 1 : 0] i_wr_data,
-    output logic                      o_wr_full,
-
-    input  logic                      i_rd_en,
-    output logic [DATA_WIDTH - 1 : 0] o_rd_data,
-    output logic                      o_rd_empty
-);
-
-    logic [DATA_WIDTH - 1 : 0] r_sram[FIFO_DEPTH - 1 : 0];
-
-    logic [PTRS_WIDTH - 1 : 0] r_wr_ptr;
-    logic [PTRS_WIDTH - 1 : 0] r_rd_ptr;
-
-    logic [PTRS_WIDTH     : 0] r_cnt;
-    logic [PTRS_WIDTH     : 0] w_cnt_nxt;
-
-    logic w_wr_full;
-    logic w_rd_empty;
-
-    // Counter
-    always_ff @(posedge i_clk or negedge i_rst_n) begin
-        if (!i_rst_n) begin
-            r_cnt <= {(PTRS_WIDTH + 1){1'b0}};
-        end
-        else if (i_wr_en && !o_wr_full) begin
-            r_cnt <= r_cnt + 1'b1;
-        end
-        else if (i_rd_en && !o_rd_empty) begin
-            r_cnt <= r_cnt - 1'b1;
-        end
-        else begin
-            r_cnt <= r_cnt;
-        end
-    end
-
-    // Write operations
-    reg_rst_y_mode_a_en_y #(
-        .DATA_WIDTH(PTRS_WIDTH)
-    ) u_reg_wr_ptr(
-        .i_clk  (i_clk),
-        .i_rst_n(i_rst_n),
-        .i_en   (i_wr_en && !o_wr_full),
-        .i_data (r_wr_ptr + 1'b1),
-        .o_data (r_wr_ptr)
-    );
-
-    integer i;
-    always_ff @(posedge i_clk or negedge i_rst_n) begin
-        if (!i_rst_n) begin
-            for (i = 0; i < FIFO_DEPTH; i++) begin
-                r_sram[i] <= {DATA_WIDTH{1'b0}};
-            end
-        end
-        else if (i_wr_en && !o_wr_full) begin
-            r_sram[r_wr_ptr] <= i_wr_data;
-        end
-        else begin
-            r_sram[r_wr_ptr] <= r_sram[r_wr_ptr];
-        end
-    end
-
-    // Read operations
-    reg_rst_y_mode_a_en_y #(
-        .DATA_WIDTH(PTRS_WIDTH)
-    ) u_reg_rd_ptr(
-        .i_clk  (i_clk),
-        .i_rst_n(i_rst_n),
-        .i_en   (i_rd_en && !o_rd_empty),
-        .i_data (r_rd_ptr + 1'b1),
-        .o_data (r_rd_ptr)
-    );
-
-    reg_rst_y_mode_a_en_y #(
-        .DATA_WIDTH(DATA_WIDTH)
-    ) u_reg_rd_data(
-        .i_clk  (i_clk),
-        .i_rst_n(i_rst_n),
-        .i_en   (i_rd_en && !o_rd_empty),
-        .i_data (r_sram[r_rd_ptr]),
-        .o_data (o_rd_data)
-    );
-
 endmodule
--- src/base/tb/fifo_tb.sv
+++ src/base/tb/fifo_tb.sv
@@ -2,7 +2,7 @@
  * @Author      : myyerrol
  * @Date        : 2024-11-02 18:09:48
  * @LastEditors : myyerrol
- * @LastEditTime: 2024-11-07 17:10:18
+ * @LastEditTime: 2024-11-09 00:48:50
  * @Description : Tools testbench.
  *
  * Copyright (c) 2024 by MEMDSL, All Rights Reserved.
@@ -19,45 +19,103 @@ end
 
 parameter DATA_WIDTH = 16;
 
-logic                      w_rst_n;
-logic                      w_wr_clk;
-logic                      w_wr_en;
-logic [DATA_WIDTH - 1 : 0] w_wr_data;
-logic                      w_rd_clk;
-logic                      w_rd_en;
+// FIFO Synchronization
+logic                      w_clk_s;
+logic                      w_rst_n_s;
+logic                      w_wr_en_s;
+logic [DATA_WIDTH - 1 : 0] w_wr_data_s;
+logic                      w_rd_en_s;
 
-// Cycle =  3ns, Freq = 333MHz
-always #1.5 w_wr_clk  = ~w_wr_clk;
-// Cycle =  2ns, Freq = 500MHz
-always #1   w_rd_clk  = ~w_rd_clk;
-// Cycle =  6ns, Freq = 166MHz
-always #3   w_wr_data = {$random} % {DATA_WIDTH{1'b1}};
+// Cycle = 1ns Freq = 1000MHz
+always #0.5 w_clk_s     = ~w_clk_s;
+// Cycle = 2ns Freq =  500MHz
+always #1   w_wr_data_s = {$random} % 10;
 
 initial begin
-    w_rst_n  = 1'b1;
+    w_rst_n_s = 1'b1;
+    w_clk_s   = 1'b0;
 
-    w_wr_clk = 1'b0;
-    w_wr_en  = 1'b0;
-    w_wr_data= {DATA_WIDTH{1'b0}};
+    w_wr_en_s   = 1'b0;
+    w_wr_data_s = {DATA_WIDTH{1'b0}};
 
-    w_rd_clk = 1'b0;
-    w_rd_en  = 1'b0;
+    #10 w_rst_n_s = 1'b0;
+    #10 w_rst_n_s = 1'b1;
 
-    # 1 w_rst_n = 0;
-    # 2 w_rst_n = 1;
+    #10 w_wr_en_s = 1'b1;
+        w_rd_en_s = 1'b0;
+    #10 w_wr_en_s = 1'b0;
+        w_rd_en_s = 1'b1;
+    #10 w_wr_en_s = 1'b1;
+        w_rd_en_s = 1'b0;
+    # 5 w_wr_en_s = 1'b1;
+        w_rd_en_s = 1'b1;
+    #10
 
-    #30 w_wr_en = 1'b1;
-        w_rd_en = 1'b0;
-    #30 w_wr_en = 1'b0;
-        w_rd_en = 1'b1;
-    #30 w_wr_en = 1'b1;
-        w_rd_en = 1'b0;
-    #30 w_rd_en = 1'b1;
+    repeat(100) begin
+        #5 w_wr_en_s = {$random} % 2;
+           w_rd_en_s = {$random} % 2;
+    end
+
+    #100 $finish;
+end
+
+fifo_mode_s #(
+    .DATA_WIDTH(DATA_WIDTH),
+    .FIFO_DEPTH(8)
+) u_fifo_s(
+    .i_clk     (w_clk_s),
+    .i_rst_n   (w_rst_n_s),
+
+    .i_wr_en   (w_wr_en_s),
+    .i_wr_data (w_wr_data_s),
+    .o_wr_full (),
+
+    .i_rd_en   (w_rd_en_s),
+    .o_rd_data (),
+    .o_rd_empty()
+);
+
+// FIFO Asynchronization
+logic                      w_rst_n_a;
+
+logic                      w_wr_clk_a;
+logic                      w_wr_en_a;
+logic [DATA_WIDTH - 1 : 0] w_wr_data_a;
+logic                      w_rd_clk_a;
+logic                      w_rd_en_a;
+
+// Cycle = 3ns, Freq = 333MHz
+always #1.5 w_wr_clk_a  = ~w_wr_clk_a;
+// Cycle = 2ns, Freq = 500MHz
+always #1   w_rd_clk_a  = ~w_rd_clk_a;
+// Cycle = 6ns, Freq = 166MHz
+always #3   w_wr_data_a = {$random} % {DATA_WIDTH{1'b1}};
+
+initial begin
+    w_rst_n_a  = 1'b1;
+
+    w_wr_clk_a = 1'b0;
+    w_wr_en_a  = 1'b0;
+    w_wr_data_a= {DATA_WIDTH{1'b0}};
+
+    w_rd_clk_a = 1'b0;
+    w_rd_en_a  = 1'b0;
+
+    #10 w_rst_n_a = 0;
+    #10 w_rst_n_a = 1;
+
+    #30 w_wr_en_a = 1'b1;
+        w_rd_en_a = 1'b0;
+    #30 w_wr_en_a = 1'b0;
+        w_rd_en_a = 1'b1;
+    #30 w_wr_en_a = 1'b1;
+        w_rd_en_a = 1'b0;
+    #30 w_rd_en_a = 1'b1;
     #30
 
     repeat (100) begin
-        #5 w_wr_en = {$random} % 2;
-           w_rd_en = {$random} % 2;
+        #5 w_wr_en_a = {$random} % 2;
+           w_rd_en_a = {$random} % 2;
     end
 
     #100 $finish;
@@ -66,16 +124,16 @@ end
 fifo_mode_a #(
     .DATA_WIDTH(DATA_WIDTH),
     .FIFO_DEPTH(8)
-) u_async_fifo(
-    .i_wr_clk  (w_wr_clk),
-    .i_wr_rst_n(w_rst_n),
-    .i_wr_en   (w_wr_en),
-    .i_wr_data (w_wr_data),
+) u_fifo_a(
+    .i_wr_clk  (w_wr_clk_a),
+    .i_wr_rst_n(w_rst_n_a),
+    .i_wr_en   (w_wr_en_a),
+    .i_wr_data (w_wr_data_a),
     .o_wr_full (),
 
-    .i_rd_clk  (w_rd_clk),
-    .i_rd_rst_n(w_rst_n),
-    .i_rd_en   (w_rd_en),
+    .i_rd_clk  (w_rd_clk_a),
+    .i_rd_rst_n(w_rst_n_a),
+    .i_rd_en   (w_rd_en_a),
     .o_rd_data (),
     .o_rd_empty()
 );
